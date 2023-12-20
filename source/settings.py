b3579010416210bcf5c977ca94a3781377da644c
Foldren <68342592+Foldren@users.noreply.github.com>
2023-12-20 17:24:21 +0300

    Update settings.py

--- source/settings.py
+++ source/settings.py
@@ -3,12 +3,8 @@ from fastapi_mail import ConnectionConfig
 
 
 MAIL_CONF = ConnectionConfig(
-    MAIL_USERNAME="schoolselectavtoshkola@gmail.com", #"Avtoshkolaselect@yandex.ru",
-    MAIL_PASSWORD="coeegulnrguxfrgb", #"fszldwozgjhvukzf",
-    MAIL_FROM="schoolselectavtoshkola@gmail.com", #"Avtoshkolaselect@yandex.ru",
     MAIL_PORT=465,
     MAIL_SERVER="smtp.gmail.com", #"smtp.yandex.ru",
-    MAIL_FROM_NAME="schoolselect",
     MAIL_STARTTLS=False,
     MAIL_SSL_TLS=True,
 )
